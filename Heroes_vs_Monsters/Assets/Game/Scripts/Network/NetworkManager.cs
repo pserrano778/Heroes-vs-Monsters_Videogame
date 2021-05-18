29a9bc2cf81de011a97c97a045e4b36b8c945006
pserrano778 <58224411+pserrano778@users.noreply.github.com>
2021-05-18 17:26:49 +0200

    inicio network

---
+++ Heroes_vs_Monsters/Assets/Game/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectedToServer();
+    }
+
+    void ConnectedToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect to Server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connect To Server.");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("Room1,", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player has joined.");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
