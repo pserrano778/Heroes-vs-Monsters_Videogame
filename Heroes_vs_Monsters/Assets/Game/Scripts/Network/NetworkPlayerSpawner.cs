ca6268373d59cdd7e4e0585d5abae7248a2bd043
pserrano778 <58224411+pserrano778@users.noreply.github.com>
2021-05-20 13:10:01 +0200

    d

---
+++ Heroes_vs_Monsters/Assets/Game/Scripts/Network/NetworkPlayerSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+    }
+}
