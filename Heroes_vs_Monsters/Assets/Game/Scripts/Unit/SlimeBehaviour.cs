46f955878fc500b12d419d4c80a0cbca8fef3767
franmolsan <56502397+franmolsan@users.noreply.github.com>
2021-06-13 14:24:27 +0200

    slime y goblin

---
+++ Heroes_vs_Monsters/Assets/Game/Scripts/Unit/SlimeBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.Linq;
+
+public class SlimeBehaviour : MonsterBehaviour
+{
+    protected override void DamageEnemy()
+    {
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(typeOfEnemy);
+
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            UnitBehaviour enemy = enemies[i].GetComponent<UnitBehaviour>();
+            print(enemy);
+
+            if ((enemy.getLane() == this.getLane() && enemy.getCurrentHealth() > 0) || 
+                (enemy.tag == "Nexus" && enemy.getCurrentHealth() > 0))
+            {
+                if ( (enemy.transform.position.x - transform.position.x >= -attackRange)
+                    ||  (enemy.transform.position.x - transform.position.x <= attackRange))
+                {
+                    enemy.takeDamage(damage);
+
+                    if (enemy.getCurrentHealth() <= 0)
+                    {
+                        enemies[i] = null;
+                    }
+                }
+            }
+        }
+
+        if (target == nexusStone && target.getCurrentHealth() > 0)
+        {
+            if ((target.transform.position.x - transform.position.x >= -attackRange)
+                || (target.transform.position.x - transform.position.x <= attackRange))
+            {
+                target.takeDamage(damage);
+
+                if (target.getCurrentHealth() <= 0)
+                {
+                    target = null;
+                }
+            }
+        }
+
+        if (target.getCurrentHealth() <= 0)
+        {
+            target = null;
+        }
+    }
+}
