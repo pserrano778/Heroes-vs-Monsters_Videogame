1da47c85309988db79987a776b417d60e5b033ba
franmolsan <56502397+franmolsan@users.noreply.github.com>
2021-06-15 21:05:44 +0200

    audio inicio

---
+++ Heroes_vs_Monsters/Assets/Game/Scripts/Settings/GameSettings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class GameSettings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+    float currentVolume;
+    Resolution[] resolutions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadSettings();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+        currentVolume = volume;
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("FullscreenPreference",
+                   Convert.ToInt32(Screen.fullScreen));
+        PlayerPrefs.SetFloat("VolumePreference",
+                   currentVolume);
+    }
+
+    public void LoadSettings()
+    {
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+        {
+            Screen.fullScreen = Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+        }  
+        else Screen.fullScreen = true;
+
+
+        if (PlayerPrefs.HasKey("VolumePreference"))
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("VolumePreference");
+        }    
+        else volumeSlider.value = 0.5f;
+    }
+
+}
---
+++ Heroes_vs_Monsters/Assets/Game/Scripts/Settings/SettingsButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsButton : MonoBehaviour
+{
+    static private Canvas settingsMenu;
+    static private Canvas mainMenu;
+
+
+    void ShowSettingsMenu()
+    {
+        mainMenu.gameObject.SetActive(false);
+        settingsMenu.gameObject.SetActive(true);
+    }
+
+    void HideSettingsMenu()
+    {
+        settingsMenu.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(true);
+    }
+
+
+}
